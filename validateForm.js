9805b64fb58a2b74e995085e1c40609eaf701fce
SP <74065235+SPchalil@users.noreply.github.com>
2020-11-25 00:11:42 -0330

    formformat

--- validateForm.js
+++ validateForm.js
@@ -1,7 +1,17 @@
 function validateForm() {
-  var x = document.forms["myForm"]["Your Name"].value;
+  var x = document.forms["myForm"]["yourName"].value;
+  var y = document.forms["myForm"]["yourEmail"].value;
+  var z = document.forms["myForm"]["yourTitle"].value;
   if (x == "") {
     alert("Name must be filled out");
+    return false;
+  }
+  if (y == "") {
+    alert("Email must be filled out");
+    return false;
+  }
+  if (z == "") {
+    alert("Title must be filled out");
     return false;
   }
 }
